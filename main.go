3f0320d5f5e6fe3c84f823d8b93b049107cb1ec0
devilethers <107105474+devilethers@users.noreply.github.com>
2022-06-08 22:19:28 +0700

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
     p, _ := proxy.New(proxy.ProxyOptions{
         Balancer: func(req *http.Request) (string, error) {
-            return "https://dev-necromeet.pantheonsite.io/", nil
+            return "https://dev-coopeuch.pantheonsite.io/", nil
         },
     })
     http.ListenAndServe(":"+os.Getenv("PORT"), p)
